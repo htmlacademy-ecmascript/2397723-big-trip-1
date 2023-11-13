7d53667e066be4a71f2fcdd5b24a8761b5447c53
AdonaiJehosua <adonaijehosua@gmail.com>
2023-11-13 20:44:48 +0500

    Добавит моковые данные и файл констант

---
+++ src/const.js
@@ -0,0 +1,85 @@
+export const FILTERS = [
+  {
+    name: 'everything',
+    isChecked: false
+  },
+  {
+    name: 'future',
+    isChecked: false
+  },
+  {
+    name: 'present',
+    isChecked: false
+  },
+  {
+    name: 'past',
+    isChecked: true
+  },
+];
+
+export const SORT_OPTIONS = [
+  {
+    name: 'day',
+    isDisable: false,
+    isChecked: true
+  },
+  {
+    name: 'event',
+    isDisable: true,
+    isChecked: false
+  },
+  {
+    name: 'time',
+    isDisable: false,
+    isChecked: false
+  },
+  {
+    name: 'price',
+    isDisable: false,
+    isChecked: false
+  },
+  {
+    name: 'offer',
+    isDisable: true,
+    isChecked: false
+  },
+];
+
+export const TYPES = [
+  {
+    type: 'taxi',
+    isChecked: false
+  },
+  {
+    type: 'bus',
+    isChecked: true
+  },
+  {
+    type: 'train',
+    isChecked: false
+  },
+  {
+    type: 'ship',
+    isChecked: false
+  },
+  {
+    type: 'drive',
+    isChecked: false
+  },
+  {
+    type: 'flight',
+    isChecked: false
+  },
+  {
+    type: 'check-in',
+    isChecked: false
+  },
+  {
+    type: 'sightseeing',
+    isChecked: false
+  },
+  {
+    type: 'restaurant',
+    isChecked: false
+  }
+];
