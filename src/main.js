212cbae1df1fc766581a1444708a804a5c07a8d0
AdonaiJehosua <adonaijehosua@gmail.com>
2023-11-13 20:49:18 +0500

    Подключит модель

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import FilterPresenter from './presenter/filter';
 import TripEventsPresenter from './presenter/trip-events';
+import EventModel from './model/event-model';
 
 const headerFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
+const eventsModel = new EventModel();
 
-const filterPresenter = new FilterPresenter({filterContainer: headerFiltersElement});
-const tripEventsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement});
+const filterPresenter = new FilterPresenter({сontainer: headerFiltersElement});
+const tripEventsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement, eventsModel: eventsModel});
 
 filterPresenter.init();
-tripEventsPresenter.initSort();
-tripEventsPresenter.initEventsList();
-tripEventsPresenter.initAddForm();
-tripEventsPresenter.initEditForm();
-tripEventsPresenter.initTripPoints();
-
+tripEventsPresenter.init();
