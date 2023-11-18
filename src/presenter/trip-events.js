e8e09c891342c22d5bc3d02650d7969825395fdc
AdonaiJehosua <adonaijehosua@gmail.com>
2023-11-18 20:48:21 +0500

    Изменит отрисовку компонентов

--- src/presenter/trip-events.js
+++ src/presenter/trip-events.js
@@ -1,25 +1,46 @@
 import { render, RenderPosition } from '../render';
 import SortView from '../view/sort';
-import EventsView from '../view/event';
-import AddFormView from '../view/add-form';
-import EditFormView from '../view/edit-form';
+import EventView from '../view/event';
+import EventsBoardView from '../view/events-board';
+import FormView from '../view/form';
 
 export default class TripEventsPresenter {
 
-  constructor({ tripEventsContainer, eventsModel }) {
+  constructor({ tripEventsContainer, eventsModel, offersModel, destinationsModel }) {
     this.tripEventsContainer = tripEventsContainer;
     this.eventsModel = eventsModel;
+    this.offersModel = offersModel;
+    this.destinationsModel = destinationsModel;
+  }
+
+  renderEvents(events) {
+    for (const event of events) {
+      render(
+        new EventView({
+          event: event,
+          offers: this.offersModel.getEventsOffers({
+            type: event.type,
+            ids: event.offers
+          }),
+          destination: this.destinationsModel.getEventsDestination(event.destination)
+        }),
+        this.tripEventsContainer
+      );
+    }
   }
 
   init() {
     this.events = [...this.eventsModel.getEvents()];
-    this.offers = [...this.eventsModel.getOffers()];
-    this.destinations = [...this.eventsModel.getDestinations()];
-    this.eventsList = new EventsView(this.events, this.offers, this.destinations);
+    this.offers = [...this.offersModel.getOffers()];
+    this.destinations = [...this.destinationsModel.getDestinations()];
+    this.eventsBoard = new EventsBoardView();
 
     render(new SortView(), this.tripEventsContainer);
-    render(this.eventsList, this.tripEventsContainer);
-    render(new EditFormView(this.events[1], this.offers, this.destinations), this.eventsList.getElement(), RenderPosition.AFTERBEGIN);
-    render(new AddFormView(this.offers, this.destinations), this.eventsList.getElement(), RenderPosition.AFTERBEGIN);
+    render(this.eventsBoard, this.tripEventsContainer);
+    this.renderEvents(this.events);
+
+
+    render(new FormView({event: this.events[1], offers: this.offers, destinations: this.destinations}), this.eventsBoard.getElement(), RenderPosition.AFTERBEGIN);
+    render(new FormView({offers: this.offers, destinations: this.destinations}), this.eventsBoard.getElement(), RenderPosition.AFTERBEGIN);
   }
 }
