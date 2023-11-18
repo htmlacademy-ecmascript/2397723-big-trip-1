e23a567c34b359a3e9cb05d43cf0c5e0a632ff9e
AdonaiJehosua <adonaijehosua@gmail.com>
2023-11-18 20:43:10 +0500

    Подключит новые модели

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import FilterPresenter from './presenter/filter';
 import TripEventsPresenter from './presenter/trip-events';
 import EventModel from './model/event-model';
+import OffersModel from './model/offers-model';
+import DestinationsModel from './model/destinations-model';
 
 const headerFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 const eventsModel = new EventModel();
+const offersModel = new OffersModel();
+const destinationsModel = new DestinationsModel();
 
-const filterPresenter = new FilterPresenter({сontainer: headerFiltersElement});
-const tripEventsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement, eventsModel: eventsModel});
+const filterPresenter = new FilterPresenter({ сontainer: headerFiltersElement });
+const tripEventsPresenter = new TripEventsPresenter({
+  tripEventsContainer: tripEventsElement,
+  eventsModel,
+  offersModel,
+  destinationsModel
+});
 
 filterPresenter.init();
 tripEventsPresenter.init();
