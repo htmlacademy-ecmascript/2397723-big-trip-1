9ea1acae92f49262a0449aad27c85e8acda3bf50
AdonaiJehosua <adonaijehosua@gmail.com>
2023-11-18 20:43:42 +0500

    Создаст новые модели

---
+++ src/model/offers-model.js
@@ -0,0 +1,21 @@
+import offers from '../mock/offers.json';
+import { getById, getByKey } from '../utils';
+
+export default class OffersModel {
+
+  offers = offers;
+
+  getOffers() {
+    return this.offers;
+  }
+
+  getEventsOffers({type, ids}) {
+    const currentOffers = getByKey('type', type, this.offers);
+    const eventsOffers = [];
+    ids.forEach((item) => (
+      item && eventsOffers.push(getById(item, currentOffers.offers))
+    ));
+    return eventsOffers;
+  }
+
+}
