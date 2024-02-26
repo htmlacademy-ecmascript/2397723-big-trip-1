5f302fc435ac9e96844f751698afe5ff17a11f83
AdonaiJehosua <adonaijehosua@gmail.com>
2024-02-26 22:56:27 +0500

    Создаст основу компонента общей информации о путешествии

--- src/presenter/trip-main-presenter.js
+++ src/presenter/trip-main-presenter.js
@@ -5,23 +5,54 @@ export default class TripMainPresenter {
   #events = [];
   #offers = [];
   #destinations = [];
+
+  #eventsModel = [];
+  #offersModel = [];
+  #destinationsModel = [];
   #tripMainContainer = null;
   #tripMainComponent = null;
 
-  constructor({ events, offers, destinations, tripMainContainer }) {
-    this.#events = events;
-    this.#offers = offers;
-    this.#destinations = destinations;
+  #title = '';
+  #duration = '';
+  #cost = '';
+  #isEmpty = false;
+
+  constructor({ eventsModel, offersModel, destinationsModel, tripMainContainer }) {
+    this.#eventsModel = eventsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
     this.#tripMainContainer = tripMainContainer;
   }
 
 
   init() {
+
+    this.#isEmpty = false;
+    this.#calculateTitle();
+    this.#calculateDuration();
+    this.#calculateCost();
     this.#renderTripMain();
   }
 
+  #calculateDuration() {
+    this.#duration = 'Too much';
+  }
+
+  #calculateCost() {
+    this.#cost = 444;
+  }
+
+  #calculateTitle() {
+    this.#title = 'Bla-bla';
+  }
+
   #renderTripMain() {
-    this.#tripMainComponent = new TripMainView();
+    this.#tripMainComponent = new TripMainView({
+      title: this.#title,
+      duration: this.#duration,
+      cost: this.#cost,
+      isEmpty: this.#isEmpty
+    });
 
     render(this.#tripMainComponent, this.#tripMainContainer, RenderPosition.AFTERBEGIN);
   }
